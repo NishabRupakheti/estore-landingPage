7f2983c3f2e82a639e71a8254c0fec4ca1407d43
Nishab <nishabrupakheti2@gmail.com>
2026-02-06 11:44:07 +0200

    Redux init

--- app/routes/cart.tsx
+++ app/routes/cart.tsx
@@ -7,9 +7,31 @@ export function meta({ }: Route.MetaArgs) {
     { name: "description", content: "This is a cart page" },
   ];
 }
+
+const items = [
+
+  { id: 1, img: "/app/images/dogfood.png", price: 100, rating: 3, reviews: 35, name: "Dog Food" },
+  { id: 2, img: "/app/images/cam.png", price: 400, rating: 4, reviews: 95, name: "DSLR Camera" },
+  { id: 7, img: "/app/images/laptop.png", price: 800, rating: 5, reviews: 325, name: "Gaming Laptop" },
+
+]
+
 const cart = () => {
   return (
-    <div>Here is a cart page</div>
+    <div>
+      {
+        items.map((item) => (
+          <div key={item.id} className="flex gap-4 mb-4 p-4 border border-gray-300 rounded">
+            <img src={item.img} alt={item.name} className="w-24 h-24 object-contain" />
+            <div>
+              <h3 className="font-semibold text-lg">{item.name}</h3>
+              <p className="text-red-500 font-semibold">${item.price}</p>
+              <p className="text-gray-600">Rating: {item.rating} ({item.reviews} reviews)</p>
+            </div>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
